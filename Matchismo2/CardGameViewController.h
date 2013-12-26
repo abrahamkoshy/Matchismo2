31372cf49d63bcebed0c4ff0652f68db7bd9d3ac
Abe Koshy <abe@ubikwity.com>
2013-12-25 21:16:56 -0600

    Initial Commit

---
+++ Matchismo2/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo2
+//
+//  Created by Abraham Koshy on 12/25/13.
+//  Copyright (c) 2013 Ubikwity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
